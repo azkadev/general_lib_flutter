0615671b87c3f21d3fdc12b52cb4064718cc73b2
￴￴ <>
2024-07-19 03:22:02 +0700

    up

--- lib/core/scroll_controller_auto_keep_state_data.dart
+++ lib/core/scroll_controller_auto_keep_state_data.dart
@@ -3,22 +3,29 @@
 import 'package:flutter/material.dart';
 import 'package:general_lib_flutter/extension/scroll_controller.dart';
 
-/// Auto Keep Scroll Controller 
+/// Auto Keep Scroll Controller
 class ScrollControllerAutoKeepStateData extends ChangeNotifier {
-  /// bool is_active or attach or scroll listener 
+  /// bool is_active or attach or scroll listener
   bool is_activate = false;
-  /// 
+
+  ///
   double last_offset = 0;
+
   /// if done call function initialized set to true
   bool _is_call_activate = false;
+
   /// page_store_key for auto save offset scroll
   late final PageStorageKey page_storage_key;
+
   /// page storage bucket for keep data scrolll
   final PageStorageBucket page_storage_bucket = PageStorageBucket();
+
   /// call only one
   void Function() on_initialized = () {};
+
   /// listen attach or detach
   void Function(bool isAttach, double offset) on_attach_or_detach = (bool isAttach, double offset) {};
+
   /// scroll_controller
   late ScrollController scroll_controller = ScrollController(
     keepScrollOffset: true,
@@ -38,6 +45,7 @@ class ScrollControllerAutoKeepStateData extends ChangeNotifier {
       update();
     },
   );
+
   /// safe offset scroll automatic without any hastle
   ScrollControllerAutoKeepStateData({
     required String keyId,
@@ -50,11 +58,12 @@ class ScrollControllerAutoKeepStateData extends ChangeNotifier {
     scroll_controller.dispose();
     super.dispose();
   }
+
   /// notify all listener
   void update() {
     notifyListeners();
   }
-  
+
   /// listen scroll
   void onScrolling({
     required bool Function() onMounted,
@@ -63,10 +72,11 @@ class ScrollControllerAutoKeepStateData extends ChangeNotifier {
     scroll_controller.onScrolling((isScrolling) {
       bool is_mounted = onMounted();
       if (is_mounted && is_activate) {
-         callback(isScrolling, scroll_controller.offset);
+        callback(isScrolling, scroll_controller.offset);
       }
     });
   }
+
   /// wrap page for auto set
   Widget build({
     required Widget child,
@@ -75,6 +85,21 @@ class ScrollControllerAutoKeepStateData extends ChangeNotifier {
       key: page_storage_key,
       bucket: page_storage_bucket,
       child: child,
+    );
+  }
+
+  /// wrap page for auto set
+  Widget builderWidget({
+    required Widget Function(BuildContext context, PageStorageBucket pageStorageBucket) builder,
+  }) {
+    return PageStorage(
+      key: page_storage_key,
+      bucket: page_storage_bucket,
+      child: Builder(
+        builder: (context) {
+          return builder(context, page_storage_bucket);
+        },
+      ),
     );
   }
 }
